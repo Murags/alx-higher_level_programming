375c2856621e4dea5729c3f7af943cd83c5b4a82
Dennis Murage <dmurage140@gmail.com>
2022-12-01 12:15:23 +0000

    Added 2-args.py projet file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if argc == 1:
     print("0 arguments.")
 elif argc == 2:
     print("1 argument:")
-    print("1 {}".format(argv[1]))
+    print("1: {}".format(argv[1]))
 else:
     print("{:d} arguments:".format(argc - 1))
     for i in range(1, argc):
-        print("{:d} {}".format(i, argv[i]))
+        print("{:d}: {}".format(i, argv[i]))
