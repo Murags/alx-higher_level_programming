9c667ff348257513e30e9c602163405618695842
Dennis Murage <dmurage140@gmail.com>
2022-12-07 09:36:54 +0000

    Error handle

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = 0
-    for i in range(len(my_list)):
+    if my_list:
+        result = 0
         j = 0
-        while j < i:
-            if my_list[j] == my_list[i]:
+        for i in range(len(my_list)):
+            j = 0
+            while j < i:
+                if my_list[j] == my_list[i]:
+                    j += 1
+                    break
                 j += 1
-                break
-            j += 1
-        if my_list[j - 1] == my_list[i]:
-            continue
-        else:
-            result += my_list[i];
-    return result
+            if my_list[j - 1] == my_list[i]:
+               continue
+            else:
+                result += my_list[i];
+        return result
