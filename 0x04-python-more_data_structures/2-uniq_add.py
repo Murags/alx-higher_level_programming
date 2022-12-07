19e4341fab86a60938bd20ef4cd7f509495fe708
Dennis Murage <dmurage140@gmail.com>
2022-12-07 10:02:31 +0000

    Added 3-common_elements.py project file

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,6 @@
 def uniq_add(my_list=[]):
     # Convert the list to a set of unique integers
     unique_ints = set(my_list)
-    
     # Convert the set back to a list and return the sum of the unique integers
     return sum(list(unique_ints))
 
