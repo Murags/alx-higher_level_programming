e98d02d3121b5ec65201c441a8553bdf61fbbf6b
Dennis Murage <dmurage140@gmail.com>
2022-12-07 09:33:47 +0000

    Added 2-uniq_add.py projet file

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    for i in range(len(my_list)):
+        j = 0
+        while j < i:
+            if my_list[j] == my_list[i]:
+                j += 1
+                break
+            j += 1
+        if my_list[j - 1] == my_list[i]:
+            continue
+        else:
+            result += my_list[i];
+    return result
