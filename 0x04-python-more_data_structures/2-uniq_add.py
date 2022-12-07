1383da14f2c95caedcc6e64694eb7c42f3f28d72
Dennis Murage <dmurage140@gmail.com>
2022-12-07 09:56:42 +0000

    Code optimization

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        result = 0
-        j = 0
-        for i in range(len(my_list)):
-            j = 0
-            while j < i:
-                if my_list[j] == my_list[i]:
-                    j += 1
-                    break
-                j += 1
-            if my_list[j - 1] == my_list[i]:
-               continue
-            else:
-                result += my_list[i];
-        return result
+    # Convert the list to a set of unique integers
+    unique_ints = set(my_list)
+    
+    # Convert the set back to a list and return the sum of the unique integers
+    return sum(list(unique_ints))
+
