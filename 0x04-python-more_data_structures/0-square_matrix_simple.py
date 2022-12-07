4f28d8ade31955845a55bc699e8ef994dee84b0b
Dennis Murage <dmurage140@gmail.com>
2022-12-07 17:13:34 +0000

    Handling styling

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     main = []
     for i in matrix:
-        sub = list(map(lambda x : x ** 2, i))
+        sub = list(map(lambda x: x ** 2, i))
         main.append(sub)
     return main
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    # Convert the list to a set of unique integers
     unique_ints = set(my_list)
-    # Convert the set back to a list and return the sum of the unique integers
     return sum(list(unique_ints))
 
