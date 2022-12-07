02fe2d8b5fd1abf4cb17d5abf24b94c7226b5fe3
Dennis Murage <dmurage140@gmail.com>
2022-12-07 17:41:19 +0000

    Fixing styling

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,4 +8,4 @@ def weight_average(my_list=[]):
        new.append((my_list[i][0] * my_list[i][1]))
        div.append(my_list[i][1])
 
-    return (sum(new) / sum(div))
+    return sum(new) / sum(div)
