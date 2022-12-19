3e3ca6b9f4c3cda2fbfbafb8f1b8d217ef6233b6
Dennis Murage <dmurage140@gmail.com>
2022-12-19 19:41:50 +0000

    fixing styling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import stderr, exc_info
 def safe_print_integer_err(value):
-    file = stderr
+    x = stderr
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        print("Exception: {}".format(exc_info()[1]), file)
+        print("Exception: {}".format(exc_info()[1]), x)
         return False
