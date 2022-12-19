f4c6cf3df289019ae97546c26a0ba1b07997627c
Dennis Murage <dmurage140@gmail.com>
2022-12-19 13:30:40 +0000

    Error handle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             except:
                 pass
         print()
-        return count
     except:
-        print()
-        return count
+        pass
+    return count
