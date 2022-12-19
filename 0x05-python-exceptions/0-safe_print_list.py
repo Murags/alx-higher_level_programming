9be07edfe8b3df0b0ad48ca773e8cb7c375c0e9e
Dennis Murage <dmurage140@gmail.com>
2022-12-19 12:15:08 +0000

    Handling edge case

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list:
-        count = 0
-        try:
-            for i in range(0,x):
-                print("{}".format(my_list[i]), end="")
-                count += 1
-            print()
-            return count
-        except:
-            print()
-            return count
-    else:
+    count = 0
+    try:
+        for i in range(0,x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
         print()
-        return 0
+        return count
+    except:
+        print()
+        return count
