80319dbf1377212b012ac605bf5c44e90d7dd207
Dennis Murage <dmurage140@gmail.com>
2022-12-19 13:38:44 +0000

    Error handle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in range(0,x):
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
         print()
         return count
-    except IndexError:
+    except:
         print()
         return count
