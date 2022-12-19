17e94e6ddbf9267c0ebde7abe4bf093a6bbbc58e
Dennis Murage <dmurage140@gmail.com>
2022-12-19 12:32:29 +0000

    Test case

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value), end="")
+        print()
         return True
-    except ValueError:
+    except:
         return False
