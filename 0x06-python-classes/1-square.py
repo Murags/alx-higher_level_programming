b2a1012715eede27d7773e6036d2721b95b0116d
Dennis Murage <dmurage140@gmail.com>
2022-12-20 10:07:30 +0000

    Added 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Daclares a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square
+
+            Args:
+                size: size of square
+
+        """
+        self.__size = size
