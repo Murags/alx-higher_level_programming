df70de90b085069b1353205dcf99d557786c57bb
Dennis Murage <dmurage140@gmail.com>
2023-01-03 10:23:40 +0000

    Added 1-rectangle file

--- 0x08-python-more_classes/1-t.py
+++ 0x08-python-more_classes/1-t.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Defines a rectangle"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
-
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """Initialize the rectangle with an optional width and height.
+
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+          width (int, optional): The width of the rectangle. Defaults to 0.
+          height (int, optional): The height of the rectangle. Defaults to 0.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """int: The width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """int: The height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
