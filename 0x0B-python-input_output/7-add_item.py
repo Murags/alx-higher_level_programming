62a8ea751d8a755972fd04872dbac37cbf997b08
Dennis Murage <dmurage140@gmail.com>
2023-01-10 14:15:29 +0000

    Error handle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,10 +7,11 @@ if __name__ == "__main__":
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+    data = []
     try:
         data = load_from_json_file("add_items.json")
     except Exception:
-        data = []
+        pass
     finally:
         for item in sys.argv[1:]:
             data.append(item)
