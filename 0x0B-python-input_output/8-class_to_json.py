d4e941830f3405ec591e814d48242b64b54078aa
Dennis Murage <dmurage140@gmail.com>
2023-01-10 13:04:33 +0000

    added 8-class_to_json.py file

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""returns the dictionary with simple data structure"""
+
 
 def class_to_json(obj):
-    return json.dumps(obj)
+    """returns the dictionary of obj"""
+    return obj.__dict__
