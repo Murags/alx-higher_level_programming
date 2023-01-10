ea1910c55de4a520f555e40e8c8146f2b96349a0
Dennis Murage <dmurage140@gmail.com>
2023-01-10 09:36:00 +0000

    Error Hanlde

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """prints contente of file"""
 
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
         f.closed
