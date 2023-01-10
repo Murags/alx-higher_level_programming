1467beb785dbb4d29158cac7b1857677b90a3cd1
Dennis Murage <dmurage140@gmail.com>
2023-01-10 21:01:49 +0000

    Error Handle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,16 @@
 
 if __name__ == "__main__":
     from sys import argv
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
 
     data = []
     try:
-        data = load_from_json_file("add_item.json")
+        data = load("add_item.json")
         for item in argv[1:]:
             data.append(item)
-        save_to_json_file(data, "add_item.json")
+        save(data, "add_item.json")
     except FileNotFoundError:
         for item in argv[1:]:
             data.append(item)
-        save_to(data, "add_item.json")
+        save(data, "add_item.json")
