2d6339de269ac8c4f62bdb3702985a737aeb73d4
Dennis Murage <dmurage140@gmail.com>
2023-01-10 20:46:20 +0000

    Fixing styling

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,7 +17,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of Student"""
+        """"""
         if isinstance(attrs, list) and\
             all(isinstance(item, str) for item in attrs):
             result = {}
