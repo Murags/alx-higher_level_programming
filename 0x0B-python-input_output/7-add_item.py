741cd1357d10b752e660d839661825adc21cf496
Dennis Murage <dmurage140@gmail.com>
2023-01-10 20:44:27 +0000

    Added 100-append_after.py file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,8 @@
 
 if __name__ == "__main__":
     from sys import argv
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     data = []
     try:
