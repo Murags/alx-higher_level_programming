c79ac7eaf16462ba71d20c29c6c2a72d9ce5eff0
Dennis Murage <dmurage140@gmail.com>
2023-01-10 11:50:17 +0000

    Added 7-add_item.py file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import json
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    data = load_from_json_file("add_items.json")
+except FileNotFoundError:
+    data = []
+
+for item in sys.argv[1:]:
+    data.append(item)
+
+save_to_json_file(data, "add_item.json")
