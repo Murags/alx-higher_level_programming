e260f08870e72252f5b7aa0540c86ec86808c7ba
Dennis Murage <dmurage140@gmail.com>
2023-01-16 14:13:29 +0000

    Updated display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,9 @@ class Rectangle(Base):
 
     def display(self):
         """Print rectangle"""
+        print("\n" * self.y, end="")
         for _ in range(self.height):
+            print(" " * self.x, end="")
             print("#" * self.width)
 
     def __str__(self):
