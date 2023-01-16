ebedcd3b4e763605937b1b4346d0a9afa711b1bf
Dennis Murage <dmurage140@gmail.com>
2023-01-16 17:45:38 +0000

    Error handle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -87,3 +87,20 @@ class Base:
                 writer = csv.DictWriter(f, fieldnames=fieldnames)
                 for i in list_objs:
                     writer.writerow(i.to_dictionary())
+
+   @classmethod
+    def load_from_file_csv(cls):
+        """Return a list"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
