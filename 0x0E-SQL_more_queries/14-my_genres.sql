979f73fc41052ffb1cb67dad9676c8623d8db0b0
Dennis Murage <dmurage140@gmail.com>
2023-02-15 23:14:34 +0300

    added task 14-16 files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
--- SELECT FORM TWO TABLES
-SELECT tv_genres.name FROM tv_show_genres WHERE tv_show_genres.show_id=8
-INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-GROUP BY tv_genres.name 
-ORDER BY tv_genres.name ASC
+-- SELECT FORM MULTIPLE TABLES
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
+WHERE tv_shows.id = 8
+ORDER BY tv_genres.name;
