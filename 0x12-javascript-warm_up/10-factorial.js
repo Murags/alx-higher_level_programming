4db1ef9983b64409389ffc8eb276dbdc3e096865
Dennis Murage <dmurage140@gmail.com>
2023-03-13 16:17:02 +0000

    task 11 files

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+const factorial = (num) => {
+  if(num != 1) {
+    return num * factorial(num - 1);
+  } else {
+    return 1;
+  }
+};
+argv[2] = isNaN(argv[2]) ? 1 : argv[2];
+console.log(factorial(+argv[2]));
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const argv = map(process.argv);
+
+
