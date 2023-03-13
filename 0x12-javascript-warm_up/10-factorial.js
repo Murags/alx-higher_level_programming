2a20409bda57aba778856672c8f55dfe1a4e9a5c
Dennis Murage <dmurage140@gmail.com>
2023-03-13 22:53:27 +0300

    Fixing error

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const argv = process.argv;
-function factorial(num){
+function factorial (num) {
   if (num !== 1) {
     return num * factorial(num - 1);
   } else {
     return 1;
   }
-};
+}
 argv[2] = isNaN(argv[2]) ? 1 : argv[2];
 console.log(factorial(+argv[2]));
