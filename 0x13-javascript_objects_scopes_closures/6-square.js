575f6244c688edc7f02cc02c019283bc759a4a9e
Dennis Murage <dmurage140@gmail.com>
2023-03-14 12:53:59 +0300

    error handle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -26,15 +26,14 @@ class Rectangle {
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c === undefined) {
       this.print();
     }else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(`${c.repeat(this.size)}`);
+      for (let i = 0; i < this.height; i++) {
+        console.log(`${c.repeat(this.width)}`);
       }
     }
   }
