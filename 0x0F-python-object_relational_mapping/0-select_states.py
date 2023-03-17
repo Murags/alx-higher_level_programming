d7a0aefea628359b79fca15307385f0c443347e5
Dennis Murage <dmurage140@gmail.com>
2023-03-17 17:13:05 +0300

    Error Handle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", port=3306,
                                     user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
     curr = database.cursor()
-    curr.execute('SELECT id, name FROM states ORDER BY id')
+    curr.execute('SELECT id, name FROM states ORDER BY id ASC')
     data = curr.fetchall()
     for state in data:
         print(state)
