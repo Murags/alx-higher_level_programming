65fd991212da479949f7fa6fc396310e1a85ce0b
Dennis Murage <dmurage140@gmail.com>
2023-03-17 16:58:54 +0300

    Error handle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,8 +4,7 @@ import MySQLdb
 from sys import argv
 
 
-def main():
-    """Print datbase items"""
+if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", port=3306,
                                     user=argv[1], passwd=argv[2], db=argv[3])
     curr = database.cursor()
@@ -16,6 +15,3 @@ def main():
     curr.close()
     database.close()
 
-
-if __name__ == "__main__":
-    main()
