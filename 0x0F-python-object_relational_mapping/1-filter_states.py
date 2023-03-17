933677e4182e886cf5fca096ad7d7d1f76873163
Dennis Murage <dmurage140@gmail.com>
2023-03-17 13:45:31 +0300

    Adding docstring

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""module to list database items"""
 import MySQLdb
 from sys import argv
 
 def main():
+    """LIst database items"""    
     database = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     curr = database.cursor()
     curr.execute('SELECT id, name FROM states WHERE name LIKE "N%" ORDER BY id')
