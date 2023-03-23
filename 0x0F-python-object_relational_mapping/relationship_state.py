a1a4a86c394f9f502f4341f467b784ac0de73aef
Dennis Murage <dmurage140@gmail.com>
2023-03-23 10:05:01 +0300

    Task 15 files

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,7 +3,7 @@
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, relationship
 from sqlalchemy import create_engine
 
 
