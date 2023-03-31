9bfb736f69644b966c437098dbcb8680f8dd132f
Dennis Murage <dmurage140@gmail.com>
2023-03-31 17:39:15 +0300

    Fixed styling

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,15 +5,16 @@ from urllib.request import urlopen
 
 def main():
     """Opens url and reads data
-    """    
+    """
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
         data = response.read()
-    
+
     print('Body response:')
     print('\t- type:', type(data))
     print('\t- content:', data)
     print('\t- utf8 content:', data.decode('utf-8'))
-        
+
+
 if __name__ == '__main__':
-    """prevent execution on import"""    
+    """prevent execution on import"""
     main()
