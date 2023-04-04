7a9b66b74ff6360e77bb5a7825d2ae608d13e02d
Dennis Murage <dmurage140@gmail.com>
2023-04-04 13:28:30 +0300

    Added 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""get user id data"""
+from requests import get
+from sys import argv
+
+
+def main():
+    repo = argv[1]
+    user = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(user, repo)
+
+    response = get(url)
+
+    data = response.json()
+    i = 0
+    for commit in data:
+        print("{} {}".format(commit["sha"], commit['commit']['author']['name']))
+        i += 1
+        if i == 10:
+            break
+
+
+if __name__ == "__main__":
+    main()
