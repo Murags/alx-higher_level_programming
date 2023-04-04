0f2e2dd94192a3767be6f8ecd800c4d232151bb1
Dennis Murage <dmurage140@gmail.com>
2023-04-04 11:52:57 +0300

    Added 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Send a POST request with a JSON body containing
+a dictionary with a key of 'q' and a value taken from the command-line arguments
+"""
+from requests import post
+from sys import argv
+
+
+def main():
+    value = ""
+    if len(argv) > 1:
+        value = argv[1]
+
+    dict_q = {"q": value}
+    response = post("http://0.0.0.0:5000/search_user", data=dict_q)
+
+    try:
+        data = response.json()
+        if len(data) == 0:
+            print("No result")
+        else:
+            id_data = data.get('id')
+            name = data.get('name')
+            print("[{}] {}".format(id_data, name))
+    except Exception:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
