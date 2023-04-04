655455673a291af47bd29ccdb890b2b83c81be64
Dennis Murage <dmurage140@gmail.com>
2023-04-04 10:34:39 +0300

    Added 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """sends post request to URL"""
 from urllib.request import urlopen
 from urllib.parse import urlencode
 from sys import argv
 
 
-url = argv[1]
-email = argv[2]
+def main():
+    """encodes email and sends post request"""
+    url = argv[1]
+    email = argv[2]
 
-email_encode = urlencode({"email": email}).encode('utf-8')
+    email_encode = urlencode({"email": email}).encode('utf-8')
 
-with urlopen(url, email_encode) as response:
-    body_response = response.read().decode('utf-8')
-    print(body_response)
+    with urlopen(url, email_encode) as response:
+        body_response = response.read().decode('utf-8')
+        print(body_response)
+
+
+if __name__ == "__main__":
+    main()
