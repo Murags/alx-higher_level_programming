6f4c7dcdb28b124f09cf8269f69275ce3e8b32f0
Dennis Murage <dmurage140@gmail.com>
2023-04-04 11:04:28 +0300

    Added 5-hbtn_header.py file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""gets request id"""
+from requests import get
+from sys import argv
+
+
+def main():
+    """Opens url and reads data
+    """
+    response = get(argv[1])
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
+
+if __name__ == '__main__':
+    """prevent execution on import"""
+    main()
